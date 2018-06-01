685d4c32e8a46dcf43bd7c0d29fea23629384b1c
Discovery Dev <discoverydev.eadcmritspdisc24@gmail.com>
2018-06-01 07:58:07 -0400

    added new drawable and added cache ing

--- app/src/main/java/com/example/ga_mlsdiscovery/dialoglistviewrecyclerview1/api/ApiServiceGenerator.java
+++ app/src/main/java/com/example/ga_mlsdiscovery/dialoglistviewrecyclerview1/api/ApiServiceGenerator.java
@@ -1,11 +1,21 @@
 package com.example.ga_mlsdiscovery.dialoglistviewrecyclerview1.api;
 
+import android.content.Context;
+
+import java.io.IOException;
+
+import okhttp3.Cache;
+import okhttp3.Interceptor;
 import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class ApiServiceGenerator {
+    Cache cache;
+    int cacheSize = 10 * 1024 * 1024; // 10MB
 
     //Singleton
     private static ApiServiceGenerator apiServiceGenerator = null;
@@ -25,11 +35,27 @@ public class ApiServiceGenerator {
     private HttpLoggingInterceptor logging = new HttpLoggingInterceptor()
             .setLevel(HttpLoggingInterceptor.Level.BODY);
 
-    private OkHttpClient.Builder httpClient = new OkHttpClient.Builder();
+    private OkHttpClient.Builder httpClient = new OkHttpClient.Builder()
+            .addInterceptor(new Interceptor() {
+                @Override
+                public Response intercept(Chain chain) throws IOException {
+                    Request request = chain.request();
+                    okhttp3.Response response = chain.proceed(request);
 
-    public <S> S createService(Class<S> serviceClass){
+                    //overwriting an existing header. use caution
+                    okhttp3.Response hackedResponse = response
+                            .newBuilder()
+                            .header("Cache-Control", "public, max-age=1")
+                            .build();
+                    return hackedResponse;
+                }
+            });
+
+    public <S> S createService(Class<S> serviceClass, Context context){
+        cache = new Cache(context.getCacheDir(), cacheSize);
         if(!httpClient.interceptors().contains(logging)){
             httpClient.addInterceptor(logging);
+            httpClient.cache(cache);
             builder.client(httpClient.build());
             retrofit = builder.build();
         }
--- app/src/main/java/com/example/ga_mlsdiscovery/dialoglistviewrecyclerview1/ui/recycler_view/RecyclerViewFragment.java
+++ app/src/main/java/com/example/ga_mlsdiscovery/dialoglistviewrecyclerview1/ui/recycler_view/RecyclerViewFragment.java
@@ -8,9 +8,17 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.DataSource;
+import com.bumptech.glide.load.engine.GlideException;
+import com.bumptech.glide.load.resource.gif.GifDrawable;
+import com.bumptech.glide.request.RequestListener;
+import com.bumptech.glide.request.RequestOptions;
+import com.bumptech.glide.request.target.Target;
 import com.example.ga_mlsdiscovery.dialoglistviewrecyclerview1.R;
 import com.example.ga_mlsdiscovery.dialoglistviewrecyclerview1.api.ApiServiceGenerator;
 import com.example.ga_mlsdiscovery.dialoglistviewrecyclerview1.api.EndPoint;
@@ -18,7 +26,6 @@ import com.example.ga_mlsdiscovery.dialoglistviewrecyclerview1.model.Photo;
 
 import java.util.ArrayList;
 import java.util.List;
-
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
@@ -29,8 +36,10 @@ import timber.log.Timber;
  */
 public class RecyclerViewFragment extends Fragment {
     private TextView mTextView;
+    private ImageView mImageView;
     private EndPoint endPoint;
     private List<Photo> photoList;
+
     public RecyclerViewFragment() {
         // Required empty public constructor
     }
@@ -41,7 +50,7 @@ public class RecyclerViewFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View v = inflater.inflate(R.layout.fragment_recycler_view, container, false);
-
+        mImageView = v.findViewById(R.id.image_view);
         mTextView = v.findViewById(R.id.test_text);
         photoList = new ArrayList<>();
         return v;
@@ -56,34 +65,80 @@ public class RecyclerViewFragment extends Fragment {
     void makeApiCall() {
         endPoint = ApiServiceGenerator
                 .getApiServiceGeneratorInstance()
-                .createService(EndPoint.class);
+                .createService(EndPoint.class, getContext());
         Call<List<Photo>> call = endPoint.getPhotosForUser();
         call.enqueue(new Callback<List<Photo>>() {
             @Override
             public void onResponse(Call<List<Photo>> call, Response<List<Photo>> response) {
                 String tempString = "";
                 if(response.isSuccessful()) {
-                    if (response.body() != null) {
+                    if (response.raw().cacheResponse()!= null) {
+                        Toast.makeText(getContext(), "cache not null", Toast.LENGTH_SHORT).show();
+//                        photoList.addAll(response.body());
+//                        for (Photo p : photoList) {
+//                            Timber.d("Photo info: " + p.getTitle() + "\n");
+//                            tempString += "Title: "+p.getTitle()+ "\n"
+//                                    +"ID: "+p.getId()+"   AlbumID: "+p.getAlbumId()+ "\n"
+//                                    +"URL: "+p.getUrl()+"\n"
+//                                    +"ThumbnailURL: "+p.getThumbnailUrl()+"\n";
+                    }
+
+                    if(response.raw().networkResponse() != null){
+                        Toast.makeText(getContext(), "network response reached", Toast.LENGTH_SHORT).show();
+
                         photoList.addAll(response.body());
                         for (Photo p : photoList) {
                             Timber.d("Photo info: " + p.getTitle() + "\n");
-                            tempString += "Title: "+p.getTitle()+ "\n"
-                                    +"ID: "+p.getId()+"   AlbumID: "+p.getAlbumId()+ "\n"
-                                    +"URL: "+p.getUrl()+"\n"
-                                    +"ThumbnailURL: "+p.getThumbnailUrl()+"\n";
+                            tempString += "Title: " + p.getTitle() + "\n"
+                                    + "ID: " + p.getId() + "   AlbumID: " + p.getAlbumId() + "\n"
+                                    + "URL: " + p.getUrl() + "\n"
+                                    + "ThumbnailURL: " + p.getThumbnailUrl() + "\n";
                         }
+
+                    }
+//https://media.giphy.com/media/3oKIPvVDlUdiBMl0dy/giphy.gif
+                    //                       Timber.d("Photo info: " + photoList.get(0).getTitle() + "\n");
+
+//                        tempString += "Title: "+photoList.get(0).getTitle()+ "\n"
+//                                +"ID: "+photoList.get(0).getId()+"   AlbumID: "+photoList.get(0).getAlbumId()+ "\n"
+//                                +"URL: "+photoList.get(0).getUrl()+"\n"
+//                                +"ThumbnailURL: "+photoList.get(0).getThumbnailUrl()+"\n";
+
+//                        Glide.with(getActivity())
+//                                .asGif()
+//                                .listener(new RequestListener<GifDrawable>() {
+//                                    @Override
+//                                    public boolean onLoadFailed(@Nullable GlideException e, Object model, Target<GifDrawable> target, boolean isFirstResource) {
+//                                        return false;
+//                                    }
+//
+//                                    @Override
+//                                    public boolean onResourceReady(GifDrawable resource, Object model, Target<GifDrawable> target, DataSource dataSource, boolean isFirstResource) {
+//                                        if(resource instanceof GifDrawable){
+//                                            ((GifDrawable)resource).setLoopCount(5);
+//                                        }
+//                                        return false;
+//                                    }
+//                                })
+//                                .load("https://media.giphy.com/media/3oKIPvVDlUdiBMl0dy/giphy.gif")
+//                                .apply(new RequestOptions()
+//                                .placeholder(R.drawable.cup_of_coffee)
+//                                .centerCrop()
+//                                .fitCenter())
+//                                .into(mImageView);
                         mTextView.setText(tempString);
                     } else {
+                        mImageView.setImageResource(R.drawable.cup_of_coffee);
                         mTextView.setText("Photos N/A");
                     }
                 }
-            }
 
             @Override
             public void onFailure(Call<List<Photo>> call, Throwable t) {
                 Toast.makeText(getContext(), "Check Network connection", Toast.LENGTH_SHORT).show();
             }
         });
+
     }
 
 }
